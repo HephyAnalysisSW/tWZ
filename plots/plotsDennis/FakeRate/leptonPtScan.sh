d2b757658a369fe7f503c4bb7e542da8d2c17d23
Dennis Schwarz <dennis.schwarz@cern.ch>
2023-03-03 17:30:49 +0100

    lep pt vs score

---
+++ plots/plotsDennis/FakeRate/leptonPtScan.sh
@@ -0,0 +1,5 @@
+python leptonPtScan.py --channel=elec --era=UL2018 
+python leptonPtScan.py --channel=muon --era=UL2018
+
+python leptonPtScan.py --channel=elec --era=ULRunII 
+python leptonPtScan.py --channel=muon --era=ULRunII
