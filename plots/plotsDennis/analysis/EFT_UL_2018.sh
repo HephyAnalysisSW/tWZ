49bb09f6284821bac6a83e348d35501ba9f69c2f
Dennis Schwarz <dennis.schwarz@cern.ch>
2023-05-23 15:54:09 +0200

    adding more sys

---
+++ plots/plotsDennis/analysis/EFT_UL_2018.sh
@@ -0,0 +1,25 @@
+# ZZ
+# python EFT_UL.py --noData --nicePlots --selection=qualepT-minDLmass12-onZ1-onZ2 --era=UL2018
+# python EFT_UL.py --noData             --selection=qualepT-minDLmass12-onZ1-onZ2 --era=UL2018
+
+# TTZ
+# python EFT_UL.py --noData --nicePlots --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018
+# python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018
+
+# WZ
+# python EFT_UL.py --noData --nicePlots --selection=trilepT-minDLmass12-onZ1-btag0-met60 --era=UL2018
+# python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-btag0-met60 --era=UL2018
+
+
+
+
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_UP_uncorrelated_2016
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_DOWN_uncorrelated_2016
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_UP_uncorrelated_2017
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_DOWN_uncorrelated_2017
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_UP_uncorrelated_2018
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_DOWN_uncorrelated_2018
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_UP_correlated_161718
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_DOWN_correlated_161718
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_UP_correlated_1718
+python EFT_UL.py --noData             --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=UL2018 --sys=Lumi_DOWN_correlated_1718
