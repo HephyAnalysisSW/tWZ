a2c5559154ae0fc3f6e5c9186b9c78cf5fa8aa0a
Dennis Schwarz <dennis.schwarz@cern.ch>
2023-08-17 16:54:34 +0200

    ttW study

---
+++ plots/plotsDennis/analysis/ttWonly.sh
@@ -0,0 +1,2 @@
+python EFT_UL.py              --reduceEFT --noData --threePoint --selection=trilepT-minDLmass12-onZ1-btag0-met60 --era=ULRunII --ttWonly
+python EFT_UL.py              --reduceEFT --noData --threePoint --selection=trilepT-minDLmass12-onZ1-njet3p-btag1p --era=ULRunII --ttWonly
