1af6ad41c3b76dcc2efba5e0100d49df50d4c697
Dennis Schwarz <dennis.schwarz@cern.ch>
2025-04-03 10:55:05 +0200

    color update

--- Tools/python/CMScolors.py
+++ Tools/python/CMScolors.py
@@ -7,10 +7,13 @@ CMScolors = {
     "tWZ":        ROOT.TColor.GetColor("#94a4a2"), # greyish-turquiose
     "ttX": ROOT.TColor.GetColor("#832db6"), # purple
     "triboson":       ROOT.TColor.GetColor("#a96b59"), # brown
-    "ZZ":  ROOT.TColor.GetColor("#e76300"), # dark orange
-    "ttZ":       ROOT.TColor.GetColor("#b9ac70"), # earthy green
     "ggZZ":      ROOT.TColor.GetColor("#717581"), # grey
-    "WZ":       ROOT.TColor.GetColor("#92dadd"), # light blue
+    # "ttZ":       ROOT.TColor.GetColor("#b9ac70"), # earthy green
+    # "WZ":       ROOT.TColor.GetColor("#92dadd"), # light blue
+    # "ZZ":  ROOT.TColor.GetColor("#e76300"), # dark orange
+    "ttZ":       ROOT.TColor.GetColor("#1984c5"), # medium blue, same as SM
+    "WZ":       ROOT.TColor.GetColor("#5ab4f9"), # light blue
+    "ZZ":  ROOT.TColor.GetColor("#1f4e79"), # dark blue
 }
 
 # gStyle->SetPalette(ROOT.kViridis)
